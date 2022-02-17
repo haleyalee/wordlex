f49c61cc27a6dda2ffd2bd0809c5dd7381e5969f
haleyalee <haleyalee@gmail.com>
2022-02-17 15:11:53 -0500

    Keyboard changes color on enter

--- Game.js
+++ Game.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Grid from './components/Board/Grid';
 import Header from './components/Header';
 import Keyboard from './components/Keyboard/Keyboard';
@@ -17,6 +17,35 @@ const styles = StyleSheet.create({
 
 const word = 'light';
 
+const keys = [{char: 'Q', color: '#818384'},
+              {char: 'W', color: '#818384'},    
+              {char: 'E', color: '#818384'},
+              {char: 'R', color: '#818384'},
+              {char: 'T', color: '#818384'},
+              {char: 'Y', color: '#818384'},
+              {char: 'U', color: '#818384'},
+              {char: 'I', color: '#818384'},
+              {char: 'O', color: '#818384'},
+              {char: 'P', color: '#818384'},
+              {char: 'A', color: '#818384'},
+              {char: 'S', color: '#818384'},
+              {char: 'D', color: '#818384'},
+              {char: 'F', color: '#818384'},
+              {char: 'G', color: '#818384'},
+              {char: 'H', color: '#818384'},
+              {char: 'J', color: '#818384'},
+              {char: 'K', color: '#818384'},
+              {char: 'L', color: '#818384'},
+              {char: 'ENTER', color: '#818384'},
+              {char: 'Z', color: '#818384'},
+              {char: 'X', color: '#818384'},
+              {char: 'C', color: '#818384'},
+              {char: 'V', color: '#818384'},
+              {char: 'B', color: '#818384'},
+              {char: 'N', color: '#818384'},
+              {char: 'M', color: '#818384'},
+              {char: '↩', color: '#818384'}];
+
 const Game = () => {
 
   const targetWord = word.toUpperCase().split('');
@@ -26,10 +55,11 @@ const Game = () => {
   const [gameLost, setGameLost] = useState(false);
   const [currentGuess, setCurrentGuess] = useState([]);
   const [colorState, setColorState] = useState([]);
+  const [keyState, setKeyState] = useState(keys);
 
   useEffect(() => {
-
-  }, [gameState]);
+    setKeyState(keys);
+  }, [keys]);
 
   const onEnter = () => {
     // Case 1: Word is not 5 letters
@@ -52,21 +82,32 @@ const Game = () => {
         const colors = [];
         currentGuess.forEach((letter, idx) => {
           const targetLetter = targetWord[idx];
+          let keyObj = {char: letter};
           // Correct: Exact match
           if (letter === targetLetter) {
             // set letter green
             colors.push('#538d4e');
+            keyObj.color = '#538d4e';
           }
           // Present: Wrong position
           else if (targetWord.includes(letter)) {
+            // check for duplicates
             // set letter yellow
             colors.push('#b59f3b');
+            keyObj.color = '#b59f3b';
           }
           // Absent: Does not contain 
           else {
             // set letter gray
             colors.push('#3a3a3c');
+            keyObj.color = '#3a3a3c';
           }
+          keys.find((letter, idx) => {
+            if (letter.char === keyObj.char) {
+              keys[idx] = keyObj;
+              return true;
+            }
+          });
         });
         setColorState([...colorState, colors]);
       }
@@ -93,7 +134,7 @@ const Game = () => {
       <StatusBar style="auto" />
       <Header />
       <Grid currentGuess={currentGuess} colorState={colorState}/>
-      <Keyboard onEnter={onEnter} onDelete={onDelete} onChar={onChar} />
+      <Keyboard onEnter={onEnter} onDelete={onDelete} onChar={onChar} keys={keyState} />
 
     </View>
   );
--- components/Keyboard/Keyboard.js
+++ components/Keyboard/Keyboard.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import GameStateContext from '../../GameStateContext';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import Key from './Key';
 
 const keyboardStyles = StyleSheet.create({
@@ -14,39 +13,9 @@ const keyboardStyles = StyleSheet.create({
   }
 });
 
-const keys = [{char: 'q', color: '#818384'},
-              {char: 'w', color: '#818384'},    
-              {char: 'e', color: '#818384'},
-              {char: 'r', color: '#818384'},
-              {char: 't', color: '#818384'},
-              {char: 'y', color: '#818384'},
-              {char: 'u', color: '#818384'},
-              {char: 'i', color: '#818384'},
-              {char: 'o', color: '#818384'},
-              {char: 'p', color: '#818384'},
-              {char: 'a', color: '#818384'},
-              {char: 's', color: '#818384'},
-              {char: 'd', color: '#818384'},
-              {char: 'f', color: '#818384'},
-              {char: 'g', color: '#818384'},
-              {char: 'h', color: '#818384'},
-              {char: 'j', color: '#818384'},
-              {char: 'k', color: '#818384'},
-              {char: 'l', color: '#818384'},
-              {char: 'enter', color: '#818384'},
-              {char: 'z', color: '#818384'},
-              {char: 'x', color: '#818384'},
-              {char: 'c', color: '#818384'},
-              {char: 'v', color: '#818384'},
-              {char: 'b', color: '#818384'},
-              {char: 'n', color: '#818384'},
-              {char: 'm', color: '#818384'},
-              {char: '↩', color: '#818384'}];
-
 const Keyboard = (props) => {
 
-  const {onEnter, onDelete, onChar} = props;
-  const {gameState, setGameState} = useContext(GameStateContext);
+  const {onEnter, onDelete, onChar, keys} = props;
   const [keyState, setKeyState] = useState(keys);
 
   const onKey = (char) => {
